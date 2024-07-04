9ab17fed3a9c56509d85eb518e7cc170f947e003
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-07-04 14:17:38 +0300

    Get locale from request

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,7 +25,7 @@ def get_locale():
 @app.route('/', strict_slashes=False)
 def home() -> str:
     """renders a simple html file"""
-    return render_template('1-index.html')
+    return render_template('2-index.html')
 
 
 if __name__ == '__main__':
